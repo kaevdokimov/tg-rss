83ca6d1abc229f96b5cfa8e12da9daaea3096676
Kirill Evdokimov <22015558+kaevdokimov@users.noreply.github.com>
2024-12-09 02:05:04 +0300

    Изменен формат сообщения для новости

--- bot/msg.go
+++ bot/msg.go
@@ -8,7 +8,7 @@ import (
 // formatNewsMessage форматирует сообщение для отправки
 func formatNewsMessage(title, link, description string, publishedAt time.Time) string {
 	return fmt.Sprintf(
-		"[*%s*](%s)\n%s\n\nОпубликовано: %s",
+		"[*%s*](%s)\n%s\n🕔 %s",
 		title, link, description, publishedAt.Format("02.01.2006 15:04"),
 	)
 }
@@ -16,7 +16,7 @@ func formatNewsMessage(title, link, description string, publishedAt time.Time) s
 // formatMessage форматирует сообщение в списке для отправки
 func formatMessage(i int, title, link, description string, publishedAt time.Time) string {
 	return fmt.Sprintf(
-		"%d. [*%s*](%s)\n%s\n\nОпубликовано: %s",
+		"%d. [*%s*](%s)\n%s\n🕔 %s",
 		i, title, link, description, publishedAt.Format("02.01.2006 15:04"),
 	)
 }
